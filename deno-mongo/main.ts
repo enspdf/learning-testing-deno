ecca40d946001bcc2e1083c43898c55685e00de1
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-05-27 22:24:06 -0500

    Deno routing, separated files, environment variables

---
+++ deno-mongo/main.ts
@@ -0,0 +1,17 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+import router from "./routes.ts";
+import notFound from "./404.ts";
+const env = config();
+console.log(env);
+
+
+const app = new Application();
+const HOST = env.APP_HOST || "http://localhost";
+const PORT = env.APP_PORT || 4000;
+
+app.use(router.routes());
+app.use(notFound);
+console.log(`server started at ${HOST}:${PORT}`);
+
+await app.listen({ port: 4000 });
---
+++ deno-mongo/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+
+router.get("/user", (ctx) => {
+    const user = { name: "Name", email: "email@email.com" };
+    ctx.response.body = user;
+});
+
+router.get("/user:id", (ctx) => {
+    ctx.response.body = ctx.params.id;
+});
+
+router.post("/user", async (ctx) => {
+    const { value } = await ctx.request.body();
+    ctx.response.status = 201;
+    ctx.response.body = value;
+});
+
+export default router;
