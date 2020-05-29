b1be5c9f066e97a718f00d192547877fb84d0e48
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-05-28 23:52:27 -0500

    GEneric validacion, added denon (similar to nodemon)

--- deno-mongo/controllers/UserController.ts
+++ deno-mongo/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import db from "../config/databases.ts";
 const users = db.collection("users");
+import validation from "../validation.ts";
 
 export default {
     async index(ctx: any) {
@@ -11,52 +12,26 @@ export default {
         ctx.response.body = data;
     },
     async store(ctx: any) {
-        if (!ctx.request.hasBody) {
-            ctx.response.status = 400;
-            ctx.response.body = { error: "Please provide the required data" }
-            return;
+        const value = await validation.validate(ctx);
+
+        if (value) {
+            const insertId = await users.insertOne(value);
+            ctx.response.status = 201;
+            ctx.response.body = insertId;
         }
-
-        const { value } = await ctx.request.body();
-
-        if (!value.email) {
-            ctx.response.status = 422;
-            ctx.response.body = { error: "Email field is required" }
-            return;
-        }
-
-        if (!value.name) {
-            ctx.response.status = 422;
-            ctx.response.body = { error: "Name field is required" }
-            return;
-        }
-
-        if (!value.password) {
-            ctx.response.status = 422;
-            ctx.response.body = { error: "Password field is required" }
-            return;
-        }
-
-        const insertId = await users.insertOne(value);
-
-        ctx.response.status = 201;
-        ctx.response.body = insertId;
     },
     async update(ctx: any) {
-        if (!ctx.request.hasBody) {
-            ctx.response.status = 400;
-            ctx.response.body = { error: "Please provide the required data" }
-            return;
+        const value = await validation.validateUpdate(ctx);
+
+        if (value) {
+            const data = { email: value.email, name: value.name, password: value.password }
+            await users.updateOne({ _id: { $oid: ctx.params.id } }, {
+                $set: data
+            });
+
+            ctx.response.status = 200;
+            ctx.response.body = { message: "Updated" }
         }
-
-        const { value } = await ctx.request.body();
-        const data = { email: value.email, name: value.name, password: value.password }
-        await users.updateOne({ _id: { $oid: ctx.params.id } }, {
-            $set: data
-        });
-
-        ctx.response.status = 200;
-        ctx.response.body = { message: "Updated" }
     },
     async destroy(ctx: any) {
         await users.deleteOne({ _id: { $oid: ctx.params.id } })
